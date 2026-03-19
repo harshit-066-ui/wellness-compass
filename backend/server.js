d5662695bec03c8839f42a360dce80f3b49cba2a
Harshit <219007351+harshit-066-ui@users.noreply.github.com>
2026-03-19 22:08:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
 import express from 'express';
 
-console.log("🚨 ROOT:", process.cwd());
-
 const app = express();
+
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('OK WORKING');
-});
+console.log("🚨 PORT:", PORT);
 
-app.get('/test', (req, res) => {
-  res.send('TEST OK');
+app.get('/', (req, res) => {
+  res.send('WORKING');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log("🚀 Listening on", PORT);
+  console.log(`🚀 Listening on ${PORT}`);
 });
